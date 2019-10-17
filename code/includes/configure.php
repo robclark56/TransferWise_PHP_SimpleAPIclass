c75a90a454cb1a6ea3d2bb8ac574c3a7ba001592
Rob <rob.clark56+github@gmail.com>
2019-10-17 14:12:56 +0800

    New commit

---
+++ code/includes/configure.php
@@ -0,0 +1,21 @@
+<?php
+//
+// Filename: .../TransferWise/includes/configure.php
+//
+
+//PRODUCTION
+define('API_KEY_TOKEN_READONLY','copy_and_paste_from_TransferWise'); 
+define('API_KEY_TOKEN_FULL'    ,'copy_and_paste_from_TransferWise'); 
+
+define('PROFILE_ID_PERSONAL','');
+define('PROFILE_ID_BUSINESS','');
+
+
+//SANDBOX
+define('SANDBOX_TOKEN_READONLY','copy_and_paste_from_TransferWise'); 
+define('SANDBOX_TOKEN_FULL'    ,'copy_and_paste_from_TransferWise'); 
+
+define('SANDBOX_ID_PERSONAL','');
+define('SANDBOX_ID_BUSINESS','');
+
+?>
