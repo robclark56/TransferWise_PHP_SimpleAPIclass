d6b31148d44c7d70bd1716c3a4fe921681e7c314
Rob <rob.clark56+github@gmail.com>
2019-10-17 13:55:08 +0800

    Update configure.php

--- code/includes/configure.php
+++ code/includes/configure.php
@@ -4,22 +4,16 @@
 //
 
 //PRODUCTION
-define('API_KEY_TOKEN_READONLY','7e54810d-a01d-471f-843f-2cf73da04986'); 
-define('API_KEY_TOKEN_FULL'    ,'c9a6d7bc-a048-48e7-9e3c-9581d747a476'); 
-
-define('PROFILE_ID_PERSONAL','10744384');
-define('PROFILE_ID_BUSINESS','10955229');
-//define('PROFILE_ID_PERSONAL','');
-//define('PROFILE_ID_BUSINESS','');
+define('API_KEY_TOKEN_READONLY','copy_and_paste_from_TransferWise'); 
+define('API_KEY_TOKEN_FULL'    ,'copy_and_paste_from_TransferWise'); 
+define('PROFILE_ID_PERSONAL','');
+define('PROFILE_ID_BUSINESS','');
 
 
 //SANDBOX
-define('SANDBOX_TOKEN_READONLY','a9f5b15b-0d7c-4e27-9003-827b26a6403a'); 
-define('SANDBOX_TOKEN_FULL'    ,'f9d36e36-ae7a-40a6-960c-49e59511bc2c'); 
-
-define('SANDBOX_ID_PERSONAL','875');
-define('SANDBOX_ID_BUSINESS','876');
-//define('SANDBOX_ID_PERSONAL','');
-//define('SANDBOX_ID_BUSINESS','');
+define('SANDBOX_TOKEN_READONLY','copy_and_paste_from_TransferWise'); 
+define('SANDBOX_TOKEN_FULL'    ,'copy_and_paste_from_TransferWise'); 
+define('SANDBOX_ID_PERSONAL','');
+define('SANDBOX_ID_BUSINESS','');
 
 ?>
