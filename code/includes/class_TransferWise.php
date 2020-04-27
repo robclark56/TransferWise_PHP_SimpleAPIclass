2005077dbe49be475026c79a8b880a66bcb34c77
Rob <rob.clark56+github@gmail.com>
2020-04-28 06:14:46 +0800

    Added missing private function createUUID()
    Whoops!
    
    Thanks Henry for pointing out.

--- code/includes/class_TransferWise.php
+++ code/includes/class_TransferWise.php
@@ -269,5 +269,15 @@ class TransferWise {
         curl_close ($ch);
         return  $response;
     }
+    
+    private function createUUID() {
+      return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+        mt_rand( 0, 0xffff ),
+        mt_rand( 0, 0x0fff ) | 0x4000,
+        mt_rand( 0, 0x3fff ) | 0x8000,
+        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+      );
+    }
 }
 ?>
