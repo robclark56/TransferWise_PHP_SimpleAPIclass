07891e71825b4fef7c8a8298cfb6ca3b87b3804a
Rob <rob.clark56+github@gmail.com>
2020-08-05 11:37:05 +0800

    Add SCA support

--- code/includes/configure.php
+++ code/includes/configure.php
@@ -9,7 +9,7 @@ define('API_KEY_TOKEN_FULL'    ,'copy_and_paste_from_TransferWise');
 
 define('PROFILE_ID_PERSONAL','');
 define('PROFILE_ID_BUSINESS','');
-
+define('PRIV_PEM'           ,'includes/private.pem');
 
 //SANDBOX
 define('SANDBOX_TOKEN_READONLY','copy_and_paste_from_TransferWise'); 
@@ -17,5 +17,5 @@ define('SANDBOX_TOKEN_FULL'    ,'copy_and_paste_from_TransferWise');
 
 define('SANDBOX_ID_PERSONAL','');
 define('SANDBOX_ID_BUSINESS','');
-
+define('SANDBOX_PRIV_PEM'   ,'includes/sandbox_private.pem');
 ?>
